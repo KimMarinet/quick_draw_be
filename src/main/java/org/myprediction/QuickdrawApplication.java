eba0051661d6ff134d23c7de3bc490f46fb7c55b
KimMarinet <kiana507680@gmail.com>
2025-07-03 11:06:35 +0900

    'start'

---
+++ src/main/java/org/myprediction/QuickdrawApplication.java
@@ -0,0 +1,13 @@
+package org.myprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickdrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickdrawApplication.class, args);
+	}
+
+}
