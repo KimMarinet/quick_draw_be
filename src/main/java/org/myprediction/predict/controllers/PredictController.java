8f5ef27e89b08d8b636c389c2ded2e32fcf4ec2b
KimMarinet <kiana507680@gmail.com>
2025-07-13 20:26:19 +0900

    quick_draw BE END

--- src/main/java/org/myprediction/predict/controllers/PredictController.java
+++ src/main/java/org/myprediction/predict/controllers/PredictController.java
@@ -2,10 +2,7 @@ package org.myprediction.predict.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.myprediction.predict.services.PredictService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -13,6 +10,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/quickdraw/predict")
+@CrossOrigin(origins="*")
 public class PredictController {
 
     private final PredictService service;
